042b03f2f9df585a9adde1a0f36ce0ca6cccf036
corey <corey.hendricks@knights.ucf.edu>
2017-04-10 23:46:41 -0400

    modified myfile2 to include gamematrix

--- Homework2/SnakesAndLadders/test_markov.cpp
+++ Homework2/SnakesAndLadders/test_markov.cpp
@@ -21,7 +21,9 @@ int main() {
 
 	// Print Results to File
 	std::ofstream myfile;
+	std::ofstream myfile2;
 	myfile.open("markov_results.txt");
+	myfile2.open("GameMatrix_results.txt");
 
 
 	// TODO add Markov vector - Matrix multiplication
@@ -32,10 +34,23 @@ int main() {
 	myfile << v << std::endl;
 	myfile << "*************" << std::endl;
 	}
+	
+	v.setZero();
+	v(0) = 1.0;
+
+	for (int i = 0; i < 101; i++) {
+		v = v.transpose() * GameMatrix;
+		for (int j = 0; j < size - 1; j++)
+		myfile2 << v(j) << std::endl;
+		std::cout << v << std::endl; //this is just a sample, becareful how you print to file so you can mine useful stats
+		myfile2 << "*************" << std::endl;
+	}
 
 	
 	
+	
 	myfile.close();
+	myfile2.close();
 
 
   return 1;
